7e853651332bde782dc8c78ee2950119033c83fb
blkshdw <aleksey.glebow@gmail.com>
2017-11-29 15:20:48 +0300

    Initial Commit

---
+++ PoloniexTestApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PoloniexTestApp
+//
+//  Created by Алексей on 29.11.17.
+//  Copyright © 2017 PoloniexTestApp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
